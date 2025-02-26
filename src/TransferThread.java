c44237df9448abe4eedcc37034276703927767bb
Vladyslav D <donetsvlad444@gmail.com>
2025-02-26 16:44:11 +0200

    lab2_2 update

--- src/TransferThread.java
+++ src/TransferThread.java
@@ -2,7 +2,7 @@ class TransferThread extends Thread {
     private final Object bank;
     private final int fromAccount;
     private final int maxAmount;
-    private static final int REPS = 1;
+    private static final int REPS = 100;
 
     public TransferThread(Object b, int from, int max) {
         bank = b;
@@ -14,9 +14,9 @@ class TransferThread extends Thread {
     public void run() {
         while (true) {
             for (int i = 0; i < REPS; i++) {
-                int toAccount = (int) (((BankSync3)bank).size() * Math.random());
+                int toAccount = (int) (((Bank)bank).size() * Math.random());
                 int amount = (int) (maxAmount * Math.random()/REPS);
-                ((BankSync3)bank).transfer(fromAccount, toAccount, amount);
+                ((Bank)bank).transfer(fromAccount, toAccount, amount);
             }
         }
     }
